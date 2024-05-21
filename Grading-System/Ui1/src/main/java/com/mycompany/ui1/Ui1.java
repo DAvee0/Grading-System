118bf65442732ba574031709dd0f591630fc4a96
Kurt Aquino <82884444+kurt0542@users.noreply.github.com>
2024-05-21 20:32:44 +0800

    Add files via upload

---
+++ Grading-System/Ui1/src/main/java/com/mycompany/ui1/Ui1.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.ui1;
+
+/**
+ *
+ * @author John Dave
+ */
+public class Ui1 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
