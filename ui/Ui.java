93012bcc4ac164b50b55159b81aea1b09dd485ba
DAvee0 <159901187+DAvee0@users.noreply.github.com>
2024-06-03 16:29:11 +0800

    Add files via upload

---
+++ ui/Ui.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.ui;
+
+/**
+ *
+ * @author John Dave
+ */
+public class Ui {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
