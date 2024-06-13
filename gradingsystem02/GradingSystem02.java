bfd2838a1683eea60ebb52f5f7d653d9a0d2ab21
DAvee0 <159901187+DAvee0@users.noreply.github.com>
2024-06-13 21:07:55 +0800

    Add files via upload

---
+++ gradingsystem02/GradingSystem02.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.gradingsystem02;
+
+/**
+ *
+ * @author Admin
+ */
+public class GradingSystem02 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
---
+++ gradingsystem02/Login.java
@@ -0,0 +1,14 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.gradingsystem02;
+
+/**
+ *
+ * @author Admin
+ */
+class Login<T0, T1> {
+    
+    
+}
